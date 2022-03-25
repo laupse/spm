d24a95894d3d5611f17ac0d771628ffad785d798
grondinl <lo.grondin@gmail.com>
2022-03-25 11:14:49 +0100

    Remove mode options from webpackconfig and block until js

--- backend-node/index.js
+++ backend-node/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-// const mysql = require("mysql2");
 const mysql = require('mysql2/promise');
 const redis = require("redis");
 
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,13 +2,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "development",
   entry: "./src/app/poulet-mayonnaise.js",
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "bundle.js",
   },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  plugins: [new HtmlWebpackPlugin({
+     template: "./src/index.html",
+     scriptLoading: "blocking"
+    })
+  ],
   module: {
     rules: [
       {
